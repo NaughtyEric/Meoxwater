3fb37e1e44a7e1a3931d8de57fd0677afd8609d7
NaughtyEric <jia_206551349@163.com>
2024-05-11 20:08:21 +0800

    修bug

--- src/plugins/custom_quotes/__init__.py
+++ src/plugins/custom_quotes/__init__.py
@@ -1,8 +1,10 @@
 import json
+import logging
 
 from nonebot import get_driver, on_fullmatch, on_command
 from nonebot.matcher import Matcher
 from nonebot.rule import CommandArg, Message
+from nonebot.log import logger
 
 from .config import Config
 from nonebot.adapters.onebot.v11 import MessageSegment, GroupMessageEvent
@@ -34,7 +36,7 @@ async def caramel(bot, event: GroupMessageEvent):
 
     # 先检查是否有对应的图片，忽略后缀
     for file in os.listdir(path + "/caramel"):
-        if re.match(f"{rnd}.*", file):
+        if re.match(f"{rnd}\..*", file):
             extention = file.split(".")[-1]
             if extention.lower() in ["jpg", "jpeg", "png", "gif", "bmp"]:
                 await caramel_event.finish(MessageSegment.image(f"file:///{path}/caramel/{file}"))
@@ -42,7 +44,7 @@ async def caramel(bot, event: GroupMessageEvent):
                 with open(f"{path}/caramel/{file}", "r") as f:
                     await caramel_event.finish(f.read())
             await caramel_event.finish(f"喵呜？第{rnd}条内容存在问题！")
-        await caramel_event.finish(f"咿呀！第{rnd}条内容不存在喵！")
+    await caramel_event.finish(f"咿呀！第{rnd}条内容不存在喵！")
 
 
 
