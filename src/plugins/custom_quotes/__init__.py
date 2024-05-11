5b4d6af3bd3ad13278d47550751a8efe021a5808
NaughtyEric <jia_206551349@163.com>
2024-05-11 19:44:10 +0800

    自定义语录功能拓展，为调整为软编码做准备

--- src/plugins/custom_quotes/__init__.py
+++ src/plugins/custom_quotes/__init__.py
@@ -19,7 +19,6 @@ config = Config.parse_obj(global_config)
 path = global_config.quote_path
 
 caramel_event = on_fullmatch(("焦糖", "caramel"), priority=5, ignorecase=True)
-caramel_said = ["捏的怎么人人都会拇指MM夕烧", "这下爆典了"]
 
 submit = on_command("submit", priority=5)
 
@@ -31,18 +30,18 @@ async def caramel(bot, event: GroupMessageEvent):
     if not os.path.exists(path + "/caramel"):
         await caramel_event.finish("暂无焦糖语录。")
     img_count = len(os.listdir(path + "/caramel"))
-    rnd = random.randint(1, img_count + len(caramel_said))
-    if rnd > img_count:
-        rnd -= img_count
-        await caramel_event.finish(caramel_said[rnd - 1])
-    else:
-        # 先检查是否有对应的图片，忽略后缀
-        for file in os.listdir(path + "/caramel"):
-            if re.match(f"{rnd}.*", file):
-                extention = file.split(".")[-1]
-                if extention.lower() not in ["jpg", "jpeg", "png", "gif", "bmp"]:
-                    await caramel_event.finish(f"喵呜？第{rnd}张图片存在问题！")
+    rnd = random.randint(1, img_count)
+
+    # 先检查是否有对应的图片，忽略后缀
+    for file in os.listdir(path + "/caramel"):
+        if re.match(f"{rnd}.*", file):
+            extention = file.split(".")[-1]
+            if extention.lower() in ["jpg", "jpeg", "png", "gif", "bmp"]:
                 await caramel_event.finish(MessageSegment.image(f"file:///{path}/caramel/{file}"))
+            elif extention.lower() in ["txt"]:
+                with open(f"{path}/caramel/{file}", "r") as f:
+                    await caramel_event.finish(f.read())
+            await caramel_event.finish(f"喵呜？第{rnd}条内容存在问题！")
         await caramel_event.finish(f"咿呀！图片不存在喵！")
 
 
--- src/plugins/custom_quotes/config.py
+++ src/plugins/custom_quotes/config.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel, Extra
 
-class Quote:
-    pass
 
 class Config(BaseModel, extra=Extra.ignore):
     """Plugin Config Here"""
+    quote_path: str
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.quote_path = self.quote_path.replace("\\", "/")
+        if not self.quote_path.endswith("/"):
+            self.quote_path += "/"
 
 
 
